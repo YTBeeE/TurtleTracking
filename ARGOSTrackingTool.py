85f0a023dbd9a23088793559b1a7c8773061a942
YTBeeE <yb97@duke.edu>
2024-09-26 15:07:12 -0400

    Create ARGO Tracking Tool

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Lab 3 in ENV 895 @ NSOE
+#
+# Author: Yuntian Bi (yb97@duke.edu)
+# Date:   September 2024
+#--------------------------------------------------------------
