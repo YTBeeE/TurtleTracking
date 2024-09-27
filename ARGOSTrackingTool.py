7aec628e5adf45aa7f1ab98047162729e59efb9b
YTBeeE <yb97@duke.edu>
2024-09-27 16:26:09 -0400

    filter data by lc

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -19,10 +19,13 @@ file_object = open(file_name,'r')
 
 lineString = file_object.readline()
 
+date_dict = {}
+location_dict = {}
+
 while lineString: 
     # as long as the lineString != "" is not null (whihc means the end of the file)
     if lineString[0] in ("#","u"):
-        print(lineString)
+        # print(lineString)
         lineString = file_object.readline()
         # to move to next line
         # because the continue skip all indented code under while loop
@@ -30,17 +33,20 @@ while lineString:
         
         continue
     
+    # lineData = lineString.split("\t") #lc is 3, lat is 5, lon is 6
     lineData = lineString.split()
-    
     # Assign variables to specfic items in the list
     record_id = lineData[0]   # ARGOS tracking record ID
     obs_date = lineData[2]   # Observation date
-    ob_lc = lineData[3]       # Observation Location Class
+    ob_lc = lineData[4]       # Observation Location Class
     obs_lat = lineData[6]     # Observation Latitude
     obs_lon = lineData[7]     # Observation Longitude   
     
     #  Print information to the use
-    # print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+    if ob_lc in ("1","2","3"):
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
+        print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
 
 
     lineString = file_object.readline()
