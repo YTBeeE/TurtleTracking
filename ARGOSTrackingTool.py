b65d94d1b944a040ab2882e199af8839015b6603
YTBeeE <yb97@duke.edu>
2024-09-27 16:15:22 -0400

    add dictionary

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -17,28 +17,19 @@ file_name = 'data/raw/Sara.txt'
 #Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file into a list
-line_list = file_object.readlines()
+lineString = file_object.readline()
 
-#Close the file
-file_object.close()
-
-# for lineString in line_list[17:]:
-# [0:15] are metatdata, starting with #
-# title is line [16], starting with uid
-# data starts from [17], which are numeric 
-# therefore, we can have a if to evaluates 
-# whether the first character of lineString occurs “in” the tuple ("#","u")
-
-for lineString in line_list:
-    if lineString[0] in ("#", "u"):
-        # print(lineString)
+while lineString: 
+    # as long as the lineString != "" is not null (whihc means the end of the file)
+    if lineString[0] in ("#","u"):
+        print(lineString)
+        lineString = file_object.readline()
+        # to move to next line
+        # because the continue skip all indented code under while loop
+        # it will never move to the next line and repeat the fist line forever 
+        
         continue
-
-
     
-    # Use the split command to parse the items in lineString into a list object
-    # lineData = lineString.split("\t")
     lineData = lineString.split()
     
     # Assign variables to specfic items in the list
@@ -46,39 +37,13 @@ for lineString in line_list:
     obs_date = lineData[2]   # Observation date
     ob_lc = lineData[3]       # Observation Location Class
     obs_lat = lineData[6]     # Observation Latitude
-    obs_lon = lineData[7]     # Observation Longitude
+    obs_lon = lineData[7]     # Observation Longitude   
     
-    # Print information to the use
-    print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+    #  Print information to the use
+    # print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
 
 
+    lineString = file_object.readline()
+    # move to the next line
+file_object.close()
 
-
-"""
-# open a file in python terminal 
-fileObj = open('data/raw/sara.txt','r')
-# read a single line 
-print(fileObj.readline())
-# Hit ↑ to recall that last command you ran (the readline one above) and run it again. 
-# You will see that Python prints the next line in the file.
-# reset the cursor back to the first line 
-# fileObj.seek(0)
-
-# read entire contents 
-lineList = fileObj.readlines()
-print(lineList[-1])
-
-# after reading, close the file 
-fileObj.close()
-
-# write a file 
-newFile = open('newfile.txt','w')
-# *Careful when writing to files as it will overwrite any existing file with that name without warning!*
-# write a string 
-newFile.write("Hello world\nIt's me")
-newFile.close()
-
-# appending to text files 
-open('newfile.txt','a').write("See what I did here")
-
-"""
